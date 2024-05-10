f9cf71a55f8cfdce20063f8ac74f5bc2f8e3d9de
LucianoEspe <lucianoespe@hotmail.com>
2024-05-10 20:37:57 -0300

    Mejora: cambio programa_id de Many2one a Many2many

--- models/inscripcion.py
+++ models/inscripcion.py
@@ -6,17 +6,26 @@ class Inscripcion(models.Model):
     _description = "aden.inscripcion"
     
     alumno_id = fields.Many2one('aden.alumno', string='Alumno', required=True)
-    programa_id = fields.Many2one('aden.programa', string='Programa', required=True)
+    programa_id = fields.Many2many('aden.programa', string='Programa', required=True) 
+    
+    # Cambio a Many2many para permitir inscribir a un alumno en varios programas a la vez
     
     @api.model
     def create(self, vals):
-        if self.inscripto(vals):
-            raise ValidationError('Este alumno ya está inscrito en este programa.')
+        if self.alumno_inscripto(vals):
+            raise ValidationError('Ya existe un registro para este alumno.')
         return super(Inscripcion, self).create(vals)
     
-    def inscripto(self, vals):
-        if self.env['aden.inscripcion'].search([
-            ('alumno_id', '=', vals.get('alumno_id')),
-            ('programa_id', '=', vals.get('programa_id'))]):
-                return True
+    # def inscripto(self, vals):
+    #     if self.env['aden.inscripcion'].search([
+    #         ('alumno_id', '=', vals.get('alumno_id')),
+    #         ('programa_id', '=', vals.get('programa_id'))]):
+    #             return True
+    #     return False
+    
+    # no necesito mas este metodo porque ahora el campo programa_id es Many2many
+    
+    def alumno_inscripto(self, vals):
+        if self.env['aden.inscripcion'].search([('alumno_id', '=', vals.get('alumno_id'))]):
+            return True
         return False
